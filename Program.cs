edc5684c2831e8c79ff4a9353f69f135115908bb
yourmaker0 <40912672+yourmaker0@users.noreply.github.com>
2018-10-26 19:40:14 +0600

    level1

--- Program.cs
+++ Program.cs
@@ -10,8 +10,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 1;
+            int y1 = 3;
+            char sym = '#';
+            Draw(x1, y1, sym);
+
+            int x2 = 4;
+            int y2 = 5;
+            Draw(x2,y2,'@');
+            Draw(7,3,'#');
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y,char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
