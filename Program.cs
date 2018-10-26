dfefaf82e8dabe7d1bbb30378aa7521ad614401c
yourmaker0 <40912672+yourmaker0@users.noreply.github.com>
2018-10-26 21:28:49 +0600

    level4

--- Program.cs
+++ Program.cs
@@ -11,14 +11,28 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            //p1.Draw();
 
             Point p2 = new Point(4,5,'@');
-            p2.Draw();
-       
+            //p2.Draw();
+
+
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            foreach(Point c in pList )
+            {
+                c.Draw();
+            }
+
             Console.ReadLine();
+
         }
 
        
+       
+
     }
 }
