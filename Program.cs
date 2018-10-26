6af57250bba8eda291d352b9a5f621d7980380b0
yourmaker0 <40912672+yourmaker0@users.noreply.github.com>
2018-10-26 19:57:58 +0600

    level2
    This reverts commit edc5684c2831e8c79ff4a9353f69f135115908bb.

--- Program.cs
+++ Program.cs
@@ -10,23 +10,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym = '#';
-            Draw(x1, y1, sym);
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            int x2 = 4;
-            int y2 = 5;
-            Draw(x2,y2,'@');
-            Draw(7,3,'#');
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '@';
+            p2.Draw();
+       
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y,char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-
-        }
+       
     }
 }
